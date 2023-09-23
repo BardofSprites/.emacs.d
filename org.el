2e8bc41d09eaf7886fc15dbb1b50c6e58eb1cae6
bard <bard@ramiel.attlocal.net>
2023-09-23 11:18:23 -0400

    more packages

---
+++ org.el
@@ -0,0 +1,40 @@
+;; |------------------------------------|
+;; |            Org Config              |
+;; |------------------------------------|
+
+(setq org-directory "~/Notes/Org-Roam/")
+(setq org-agenda-files (list "~/Notes/Org-Roam/todo.org"))
+(setq org-roam-directory "~/Notes/Org-Roam/")
+(setq org-roam-db-autosync t)
+
+(setq org-agenda-custom-commands
+      `(("A" "Daily agenda and top priority tasks"
+         ((tags-todo "*"
+                     ((org-agenda-skip-function '(org-agenda-skip-if nil '(timestamp)))
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                      (org-agenda-overriding-header "All Tasks \n")))
+          (agenda "" ((org-agenda-span 1)
+                      (org-agenda-start-day nil)
+                      (org-deadline-warning-days 0)
+                      (org-scheduled-past-days 0)
+                      ;; We don't need the `org-agenda-date-today'
+                      ;; highlight because that only has a practical
+                      ;; utility in multi-day views.
+                      (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+                      (org-agenda-format-date "%A %-e %B %Y")
+                      (org-agenda-overriding-header "Today's agenda \n")))
+          ;; write skip function that skips saturdays and sundays
+          (agenda "" ((org-agenda-span 7)
+                      (org-deadline-warning-days 0)
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                      (org-agenda-overriding-header "Upcoming this week \n")))))
+        ("Y" "Monthly view for all tasks"
+         ((agenda "" ((org-agenda-span 365)
+                      (org-deadline-warning-days 2)
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                      (org-agenda-overriding-header "Upcoming this Year\n")))))
+        ("S" "Monthly view for all tasks"
+         ((agenda "" ((org-agenda-span 31)
+                      (org-deadline-warning-days 2)
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                      (org-agenda-overriding-header "Upcoming this month\n")))))))
