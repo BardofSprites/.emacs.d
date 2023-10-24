7211aa562b716776756493559b8138bc45ce44c1
bard <bard@ramiel.attlocal.net>
2023-10-24 06:59:38 -0400

    more helm gtags customization

--- bard-emacs-modules/bard-emacs-package.el
+++ bard-emacs-modules/bard-emacs-package.el
@@ -258,7 +258,19 @@
   (add-hook 'dired-mode-hook 'helm-gtags-mode)
   (add-hook 'eshell-mode-hook 'helm-gtags-mode)
   (add-hook 'c-mode-hook 'helm-gtags-mode)
-  (add-hook 'c++-mode-hook 'helm-gtags-mode))
+  (add-hook 'c++-mode-hook 'helm-gtags-mode)
+
+  (setq
+   helm-gtags-ignore-case t
+   helm-gtags-auto-update t
+   helm-gtags-use-input-at-cursor t
+   helm-gtags-pulse-at-cursor t
+   helm-gtags-prefix-key "\C-cg"
+   helm-gtags-suggested-key-mapping t)
+  
+  (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim))
 
 (use-package aweshell
   :quelpa (abc-mode :fetcher github :repo "manateelazycat/aweshell")
