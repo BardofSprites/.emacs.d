5782d195e21e06960cad12f0677ed3c7f6ec45a7
bard <bard@ramiel.attlocal.net>
2024-02-08 17:59:03 -0500

    open terminal in dired

--- bard-elisp/bard-terminal.el
+++ bard-elisp/bard-terminal.el
@@ -6,4 +6,12 @@
 
 (global-set-key (kbd "C-c t") 'bard/open-terminal-in-current-directory)
 
+(defun bard/open-terminal-emulator ()
+  "Open a terminal in the current working directory."
+  (interactive)
+  (let ((default-directory default-directory))
+    (start-process "st terminal" nil "st")))
+
+(define-key dired-mode-map (kbd "C-c C-t") 'bard/open-terminal-emulator)
+
 (provide 'bard-terminal.el)
