958d6b2d0e0621c0b6fad550d1fbc7cfa454cfbb
bard <bard@ramiel.attlocal.net>
2024-03-07 07:15:11 -0500

    more accessible keybinds

--- bard-elisp/bard-terminal.el
+++ bard-elisp/bard-terminal.el
@@ -4,7 +4,7 @@
   (let ((default-directory default-directory))
     (term "/bin/bash")))
 
-(global-set-key (kbd "C-c t") 'bard/open-terminal-in-current-directory)
+(define-key global-map (kbd "C-t") #'bard/open-terminal-in-current-directory)
 
 (defun bard/open-terminal-emulator ()
   "Open a terminal in the current working directory."
@@ -12,7 +12,6 @@
   (let ((default-directory default-directory))
     (start-process "st terminal" nil "st")))
 
-(with-eval-after-load "dired-mode"
-  (define-key dired-mode-map (kbd "C-c C-t") 'bard/open-terminal-emulator))
+(define-key global-map (kbd "C-c t") 'bard/open-terminal-emulator)
 
 (provide 'bard-terminal.el)
