330cab74d22e4f4c2714c9249adedce69c478433
bard <bard@ramiel.attlocal.net>
2024-03-11 07:15:07 -0400

    browse-url-librewolf

--- bard-emacs-modules/bard-emacs-web.el
+++ bard-emacs-modules/bard-emacs-web.el
@@ -17,3 +17,28 @@
         "\\`\\(video/\\|audio\\)")
 
 (global-set-key (kbd "C-c w") 'eww)
+
+;; librewolf open browser
+
+(defun browse-url-librewolf (url &optional new-window)
+  "Ask the Librewolf WWW browser to load URL.
+Defaults to the URL around or before point.  Passes the strings
+in the variable `browse-url-librewolf-arguments' to Librewolf.
+
+Interactively, if the variable `browse-url-new-window-flag' is non-nil,
+loads the document in a new Librewolf window.  A non-nil prefix argument
+reverses the effect of `browse-url-new-window-flag'.
+
+If `browse-url-librewolf-new-window-is-tab' is non-nil, then
+whenever a document would otherwise be loaded in a new window, it
+is loaded in a new tab in an existing window instead.
+
+Non-interactively, this uses the optional second argument NEW-WINDOW
+instead of `browse-url-new-window-flag'."
+  (interactive (browse-url-interactive-arg "URL: "))
+  (setq url (browse-url-encode-url url))
+  (let* ((process-environment (browse-url-process-environment)))
+    (apply #'start-process
+           (concat "librewolf " url) nil
+           "librewolf"
+            (list url))))
