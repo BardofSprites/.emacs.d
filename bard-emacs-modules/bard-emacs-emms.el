e6663948bac241230b734adca9a10432714a338c
bard <bard@ramiel.attlocal.net>
2024-03-11 21:52:21 -0400

    new recenter function

--- bard-emacs-modules/bard-emacs-emms.el
+++ bard-emacs-modules/bard-emacs-emms.el
@@ -17,6 +17,12 @@
 
 (setq emms-volume-amixer-card 1)
 
+;; center line function
+(defun bard/emms-recenter ()
+  (interactive)
+  (recenter-top-bottom)
+  (emms-playlist-mode-center-current))
+
 ;; modeline
 (emms-mode-line-disable)
 (emms-playing-time-disable-display)
@@ -30,5 +36,6 @@
 (define-key emms-playlist-mode-map (kbd "L") #'emms-toggle-repeat-track)
 (define-key emms-playlist-mode-map (kbd "<mouse-3>") #'emms-pause)
 (define-key emms-playlist-mode-map (kbd "<SPC>") #'emms-pause)
+(define-key emms-playlist-mode-map (kbd "c") #'bard/emms-recenter)
 
 (provide 'bard-emacs-emms)
