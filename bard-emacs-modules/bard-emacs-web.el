92ece3edb9d089b2227c74d18bbe2f15ef24b53e
bard <bard@ramiel.attlocal.net>
2024-03-22 21:47:43 -0400

    url handler rules

--- bard-emacs-modules/bard-emacs-web.el
+++ bard-emacs-modules/bard-emacs-web.el
@@ -1,11 +1,19 @@
 (setq browse-url-browser-function 'eww-browse-url)
 (setq browse-url-secondary-browser-function 'browse-url-default-browser)
 
-(setq shr-use-colors nil)             ; t is bad for accessibility
-(setq shr-use-fonts nil)              ; t is not for me
+(setq
+ browse-url-handlers
+ '(("wikipedia\\.org" . eww-browse-url)
+   ;; ("github" . browse-url-chromium)
+   ("github" . browse-url-default-browser)
+   ("reddit.com" . browse-url-default-browser)))
+
+;; shr optimizations
+(setq shr-use-colors nil)
+(setq shr-use-fonts nil)
 (setq shr-max-image-proportion 0.6)
-(setq shr-image-animate nil)          ; No GIFs, thank you!
-(setq shr-width fill-column)          ; check `prot-eww-readable'
+(setq shr-image-animate nil)
+(setq shr-width fill-column)
 (setq shr-max-width fill-column)
 (setq shr-discard-aria-hidden t)
 (setq shr-cookie-policy nil)
