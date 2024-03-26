53819b7be6e212f67e4ab74c3b86e807ab202eb7
bard <bard@ramiel.attlocal.net>
2024-03-26 18:01:06 -0400

    modularize and change banner

--- bard-elisp/bard-eshell.el
+++ bard-elisp/bard-eshell.el
@@ -79,12 +79,4 @@ open the directory in a `dired' buffer."
             ((not in-eshellp) (switch-to-buffer (car buffers)))
             (t (select-or-create (completing-read "Select Shell:" (cons "New eshell" names)))))))
 
-(global-set-key (kbd "C-z") nil)
-(define-key global-map (kbd "C-z e") #'eshell-switcher)
-(with-eval-after-load "esh-mode"
-  (define-key eshell-mode-map (kbd "C-c f") #'bard/eshell-find-file-at-point)
-  (define-key eshell-mode-map (kbd "C-c h") #'prot-eshell-narrow-output-highlight-regexp)
-  (define-key eshell-mode-map (kbd "C-c d") #'prot-eshell-complete-recent-dir)
-  (define-key eshell-mode-map (kbd "M-k") #'eshell-kill-input))
-
 (provide 'bard-eshell.el)
---
+++ bard-emacs-modules/bard-emacs-eshell.el
@@ -0,0 +1,14 @@
+(use-package eshell
+  :ensure t
+  :config
+  (setq eshell-banner-message "Time for another recreational programming session.\n\n"))
+
+(global-set-key (kbd "C-z") nil)
+(define-key global-map (kbd "C-z e") #'eshell-switcher)
+(with-eval-after-load "esh-mode"
+  (define-key eshell-mode-map (kbd "C-c f") #'bard/eshell-find-file-at-point)
+  (define-key eshell-mode-map (kbd "C-c h") #'prot-eshell-narrow-output-highlight-regexp)
+  (define-key eshell-mode-map (kbd "C-c d") #'prot-eshell-complete-recent-dir)
+  (define-key eshell-mode-map (kbd "M-k") #'eshell-kill-input))
+
+(provide 'bard-emacs-eshell.el)
