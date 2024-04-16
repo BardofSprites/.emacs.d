b139c8ff5cd3d784d3b462012a1911f00b6c4542
bard <bard@ramiel.attlocal.net>
2024-04-16 06:15:23 -0400

    old ada mode

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 
 ;; Add the directories to the load path
 (add-to-list 'load-path "~/.emacs.d/elisp/")
+(add-to-list 'load-path "~/.emacs.d/old-ada/")
 (add-to-list 'load-path (expand-file-name "bard-elisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "bard-emacs-modules" user-emacs-directory))
 
