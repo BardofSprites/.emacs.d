590c1b82eae23792f3f7c41a0a69d65d6c3760b8
bard <bard@ramiel.attlocal.net>
2024-05-08 06:24:06 -0400

    emacs snippets

--- tempel-snippets.el
+++ tempel-snippets.el
@@ -4,3 +4,9 @@ fundamental-mode ;; Available everywhere
 (NOW (format-time-string "<%Y-%0m-%0d %a %H:%0M>"))
 (yesterday (format-time-string "<%Y-%0m-%0d %a>" (time-subtract nil (* 24 60 60))))
 (tomorrow (format-time-string "<%Y-%0m-%0d %a>" (time-add nil (* 24 60 60))))
+
+emacs-lisp-mode
+
+(up "(use-package "p n> (s configuration)")")
+
+(key "(define-key " p " (kbd \""p "\") #'"p")")
