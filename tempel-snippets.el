2a53e5e6745032ba30796a8efa03ce791e27b6fb
bard <bard@ramiel.attlocal.net>
2024-05-17 06:13:51 -0400

    hook and file mode snippets

--- tempel-snippets.el
+++ tempel-snippets.el
@@ -4,9 +4,12 @@ fundamental-mode ;; Available everywhere
 (NOW (format-time-string "<%Y-%0m-%0d %a %H:%0M>"))
 (yesterday (format-time-string "<%Y-%0m-%0d %a>" (time-subtract nil (* 24 60 60))))
 (tomorrow (format-time-string "<%Y-%0m-%0d %a>" (time-add nil (* 24 60 60))))
+(mode "-*- mode: " (s mode) " -*-")
 
 emacs-lisp-mode
 
 (up "(use-package "p n> (s configuration)")")
 
 (key "(define-key " p " (kbd \""p "\") #'"p")")
+
+(hook "(add-hook '"(s hook-name) " #'" (s function-to-add) ")")
