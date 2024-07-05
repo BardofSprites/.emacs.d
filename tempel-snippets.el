54ef879a8bd173428276395af4d39e873d094435
Daniel <89086143+BardofSprites@users.noreply.github.com>
2024-07-04 23:13:08 -0400

    template for khan academy units

--- tempel-snippets.el
+++ tempel-snippets.el
@@ -6,6 +6,13 @@ fundamental-mode ;; Available everywhere
 (tomorrow (format-time-string "<%Y-%0m-%0d %a>" (time-add nil (* 24 60 60))))
 (mode "-*- mode: " (s mode) " -*-")
 
+org-mode
+
+(title "#+title: "p n "#+date: "(format-time-string "[%Y-%0m-%0d %a %H:%0M]") n)
+(subunit "*** " (s title) " [/]" n "**** TODO lesson" p n "**** TODO worked example" p n "**** TODO quick example" p n "**** TODO practice" p)
+
+(eg "(e.g. " p ")")
+
 emacs-lisp-mode
 
 (up "(use-package "p n> ":ensure t" n> (s configuration)")")
