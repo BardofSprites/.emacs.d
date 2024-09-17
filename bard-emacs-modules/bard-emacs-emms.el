34c0daa8a43acb1391d95567dfccde9fadd6abe7
BardofSprites <89086143+BardofSprites@users.noreply.github.com>
2024-09-16 21:42:46 -0400

    more use-packagey for emms

--- bard-emacs-modules/bard-emacs-emms.el
+++ bard-emacs-modules/bard-emacs-emms.el
@@ -2,46 +2,48 @@
 ;; |             EMMS                   |
 ;; |------------------------------------|
 
-(require 'emms)
-(require 'emms-setup)
-(require 'emms-mpris)
-(require 'emms-mode-line-icon)
+;; (require 'emms)
+;; (require 'emms-setup)
+;; (require 'emms-mpris)
+;; (require 'emms-mode-line-icon)
 
-(emms-all)
-(emms-default-players)
-(emms-mpris-enable)
-(setq emms-player-list '(emms-player-mpv)
-      emms-info-functions '(emms-info-native))
-
-(setq emms-browser-covers 'emms-browser-cache-thumbnail)
-
-(setq emms-volume-amixer-card 0)
-
-;; center line function
-(defun bard/emms-recenter ()
-  (interactive)
-  (recenter-top-bottom)
-  (emms-playlist-mode-center-current))
-
-;; modeline
-(emms-mode-line-disable)
-(emms-playing-time-disable-display)
-
-;; Keymaps
 (use-package emms
+  :ensure t
+  ;; :demand t
   :bind
   (:map emms-playlist-mode-map
-   ("A" . emms-add-directory)
-   ("T" . emms-add-directory-tree)
-   ("F" . emms-add-file)
-   ("U" . emms-add-url)
-   ("L" . emms-toggle-repeat-track)
-   ("<mouse-3>" . emms-pause)
-   ("<SPC>" . emms-pause)
-   ("c" . bard/emms-recenter))
+        ("A" . emms-add-directory)
+        ("T" . emms-add-directory-tree)
+        ("F" . emms-add-file)
+        ("U" . emms-add-url)
+        ("L" . emms-toggle-repeat-track)
+        ("<mouse-3>" . emms-pause)
+        ("<SPC>" . emms-pause)
+        ("c" . bard/emms-recenter))
   :bind (("<f8>" . emms)
          ("M-<f8>" . emms-browser))
   :hook
-  (emms-playlist-mode . hl-line-mode))
+  (emms-playlist-mode . hl-line-mode)
+  :config
+  (emms-all)
+  (emms-default-players)
+  (emms-mpris-enable)
+  (setq emms-player-list '(emms-player-mpv))
+  ;; emms-info-functions '(emms-info-native)
+
+  ;; (setq emms-browser-covers 'emms-browser-cache-thumbnail)
+
+  (setq emms-volume-amixer-card 0)
+
+  ;; center line function
+  (defun bard/emms-recenter ()
+    (interactive)
+    (recenter-top-bottom)
+    (emms-playlist-mode-center-current))
+
+  ;; modeline
+  (emms-mode-line-disable)
+  (emms-playing-time-disable-display)
+  )
 
 (provide 'bard-emacs-emms)
