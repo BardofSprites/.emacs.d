aa800234c4227b71082d99a388fa510051a67d79
BardofSprites <bard>
2025-10-24 08:34:58 -0400

    new denote buffer prefix

--- bard-emacs-modules/bard-emacs-writing.el
+++ bard-emacs-modules/bard-emacs-writing.el
@@ -61,7 +61,8 @@
   :config
   (setq denote-directory "~/Notes/denote/")
   (setq denote-journal-extras-directory "~/Notes/journal")
-  (setq denote-rename-buffer-format "[Note] %t %b")
+  (setq denote-rename-buffer-format "%s %t"
+        denote-buffer-name-prefix "[Note] ")
   (setq denote-known-keywords
         '("emacs"
           "linux"
